5f18dd60d482e47b72cd49622199875107d637fd
Max Milton <max@wearegenki.com>
2021-06-17 13:53:51 +1000

    Optimise function for file size

--- src/content.ts
+++ src/content.ts
@@ -5,7 +5,7 @@ declare global {
   var trackx: typeof TrackX; // eslint-disable-line no-var, vars-on-top
 }
 
-function init() {
+const init = () => {
   trackx.setup('https://api.trackx.app/v1/pxdfcbscygy/event');
   trackx.addMeta('release', process.env.APP_RELEASE);
   trackx.addMeta('agent', 'harvest-errors');
@@ -14,7 +14,7 @@ function init() {
   if (process.env.NODE_ENV !== 'production') {
     trackx.addMeta('NODE_ENV', process.env.NODE_ENV);
   }
-}
+};
 
 const script = document.createElement('script');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
