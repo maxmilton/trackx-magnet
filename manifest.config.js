c20669e831a2e05c75dfd31b6639c3b5edd1f302
Max Milton <max@wearegenki.com>
2021-06-17 12:58:20 +1000

    Atempt to fix version name is automated publish flow

--- manifest.config.js
+++ manifest.config.js
@@ -12,7 +12,7 @@ const manifest = {
   name: 'Harvest Errors',
   description: 'Collect errors from web pages with the trackx client.',
   version: pkg.version,
-  version_name: gitRef(),
+  version_name: process.env.GITHUB_REF || gitRef(),
   icons: {
     16: 'icon16.png',
     48: 'icon48.png',
