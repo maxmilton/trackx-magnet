e47fb40051184c297327de52533f212a2a2be580
Max Milton <max@happyhackers.xyz>
2023-12-21 13:23:37 +1100

    chore: Tweaks and comments

--- src/magnet.ts
+++ src/magnet.ts
@@ -11,11 +11,9 @@
 import type { EventMeta } from 'trackx/types';
 import type { CaptureData } from './types';
 
-// TODO: Force disable `bun-types` for this file and use `esnext` lib instead
-// eslint-disable-next-line no-var
-declare var addEventListener: Window['addEventListener'];
-// eslint-disable-next-line no-var
-declare var postMessage: Window['postMessage'];
+// TODO: Fix bun types overriding dom types
+declare let addEventListener: Window['addEventListener'];
+declare let postMessage: Window['postMessage'];
 
 // Same as https://github.com/maxmilton/trackx/blob/master/packages/trackx/src/modern.ts#L27-L30
 // TODO: Use enum once bun support inlining them; https://github.com/oven-sh/bun/issues/2945
@@ -34,6 +32,9 @@ const EventTypeConsoleError = 3;
 //  ↳ https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm#error_types
 
 const capture = (type: number, error: unknown, extra?: EventMeta): void =>
+  // FIXME: Send via postMessage fails when error is or contains DOM nodes or
+  // a function reference (due to the structured clone algorithm).
+  // ↳ https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm#things_that_dont_work_with_structured_clone
   postMessage({
     x_x: true,
     $$type: type,
